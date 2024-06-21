a28517d8fefdee76e7ea5bd26a893d0616f266ef
codegeekq <161741095+codegeekq@users.noreply.github.com>
2024-06-21 08:42:53 +0200

    Add files via upload

---
+++ pages/API.js
@@ -0,0 +1,184 @@
+let ahdev = 'Ahmad'
+export const articlesArray = [
+  {},
+  {
+    poster: ahdev,
+    articleName: 'HTML Introduction (Lesson 1)',
+    date: '7 / 10 / 2023 ',
+    article: ' Welcome into HTML introduction <br> In the beginning HTML is a markup language used to build the structure of  websites pages and this language is the farst and main step in your  programming learning trip . <br>  HTML founded by Tim Berners-Lee in 1993 and this language has passed in several stages the first of them is HTML 1.0 then HTML 2.0 , HTML 3.0 , HTML 4.0 and last version is HTML 5.0 . <br> This language is very easy and you can use it with more languages as : CSS , JS , Python , PHP ... etc . And we will study this language in general and I will explain some of main tags which helps you to build websites pages easily . <br> And when you finish learning this language you can learn CSS in our website <a href="../css/lesson1.html">by this link</a> .<br><br> And now we will start learning the HTML . First this language depends on your understanding of this information: this language has many tags as <br> &lt;html&gt; &lt;body&gt; &lt;head&gt; ... and very more tags used in different sites . <br> And this image explain the tag structure <br> <img src="https://tutorial.techaltum.com/images/element.png" alt="HTML TAG" height="130px" style="  border-radius: 10px;filter: invert(100%)"> <br> Now you have this table that contains HTML tags . <br> <br> <h5 class="headers">HTML 5 Tags & Usage</h5> <br> <iframe style="width:100%;height:1000px" src="./html tags .html" frameborder="0"></iframe> <br> By this table you can create any page structure fast <br> This lesson has finished. You can continue the second lesson using this link <a href="./lesson2.html">👉 Lesson 2 👈</a>',
+    shortArticle: 'Welcome into HTML introduction ... In the beginning HTML is a markup language used to build the structure of  websites pages and this language is the farst and main step in your  programming learning trip .',
+    learnMoreLink: 'pages/Front-end Road Map/html/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'HTML',
+    articleImag: '/Images/Articles Images /html.png' ,
+    lessonNumer: 'leeson 1' ,
+    roadMap : 'Front-end',
+    
+    
+    arArticle: 
+      `مرحبًا بكم في HTML مقدمة <br> في البداية HTML هي لغة ترميز تستخدم لبناء هيكل صفحات مواقع الويب وهذه اللغة هي الخطوة الأبعد والأساسية في رحلة تعلم البرمجة الخاصة بك. <br> HTML أسسها تيم بيرنرز لي في عام 1993 وقد مرت هذه اللغة في عدة مراحل ، أولها هو HTML 1.0 ثم HTML 2.0 و HTML 3.0 و HTML 4.0 والإصدار الأخير هو HTML 5.0. <br> هذه اللغة سهلة للغاية ويمكنك استخدامها بمزيد من اللغات مثل: css و js و python و php ... إلخ. وسندرس هذه اللغة بشكل عام وسأشرح بعض العلامات الرئيسية التي تساعدك على إنشاء صفحات مواقع الويب بسهولة. <br> وعند الانتهاء من تعلم هذه اللغة ، يمكنك تعلم CSS في موقعنا <a href="../css/lesson1.html"> من خلال هذا الرابط </a>. <br> <br> والآن سنبدأ في تعلم HTML. أولاً ، تعتمد هذه اللغة على فهمك لهذه المعلومات: تحتوي هذه اللغة على العديد من العلامات مثل <br> & lt؛ html & gt؛ & lt؛ الجسم & gt؛ & lt؛ head & gt؛ ... والمزيد من العلامات المستخدمة في مواقع مختلفة. <br> وهذه الصورة تشرح بنية العلامة <br> <img src = "https://tutorial.techaltum.com/images/element.png" alt="HTML tag" height = "130px" style = "border-radius: 10px؛ filter: INVERT(100%)"> <br> لديك هذا الجدول الذي يحتوي على علامات HTML. <br> <br> <h5 class = "headers"> html 5 علامات & الاستخدام </ h5> <br> <iframe style = "width: 100٪؛ height: 1000px" src = "./tags .html" FrameBorder = "0"> </iframe> <br> من خلال هذا الجدول ، يمكنك إنشاء أي بنية صفحة بسرعة <br> انتهى هذا الدرس. يمكنك متابعة الدرس الثاني باستخدام هذا الرابط <a href="./lesson2.html"> الدرس 2 👈</a>`
+    , 
+    
+    
+    searchKeyWords: 'Lesson Tutorial of html Introduction intro Intro introduction lesson1 web fornt Front end development html tags basics HTML Html first lesson on html tutorial ' ,
+  },
+  {
+    poster: ahdev,
+    articleName: 'HTML Lesson 2',
+    date: '22 / 10 / 2023 ',
+    article: `In the preceding HTML lesson, we grasped the basics; now, we ascend to conceptualizing websites before coding, emphasizing the evolving skill of envisioning projects.<br> Consider the purpose, draw inspiration from similar sites, and infuse your unique touches. This process ensures creativity blossoms, transcending the conventional. <br> To enhance your HTML programming prowess, delve into the art of effective research. Explore online resources, tutorials, and documentation to deepen your understanding of HTML tags, attributes, and best practices. <br> Familiarize yourself with semantic HTML to imbue meaning and structure into your web pages, enhancing accessibility and search engine optimization. <br> Practice makes perfect, so immerse yourself in coding exercises and projects to hone your skills. <br> Experiment with different layouts, styles, and techniques to broaden your repertoire. Seek feedback from peers and mentors to gain valuable insights and refine your approach. <br> Furthermore, mastering the art of page structure is paramount. <br> Learn how to organize content logically using headings, paragraphs, lists, and other structural elements. Utilize HTML5 semantic elements like &lt;header&gt;, &lt;nav&gt;, &lt;main&gt;, &lt;section&gt;, and <footer> to create well-structured, readable, and maintainable code. <br> Stay updated with industry trends and emerging technologies to stay ahead of the curve. Explore frameworks like Bootstrap and libraries like jQuery to streamline development and enhance productivity.<br> Embrace continuous learning and experimentation to evolve as a proficient HTML developer.<br><br>Conclude the lesson and continue your journey with <a href="./lesson3.html">👉 Lesson 3 👈</a>. Happy coding!` ,
+    shortArticle: `In the preceding HTML lesson, we grasped the basics; now, we ascend to conceptualizing websites before coding, emphasizing the evolving skill of envisioning projects.` ,
+    learnMoreLink: 'pages/Front-end Road Map/html/lesson2.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'HTML',
+    articleImag: '/Images/Articles Images /html.png' ,
+    lessonNumer: 'leeson 2' ,
+    roadMap : 'Front-end',
+    searchKeyWords: 'Lesson html Front end front lesson2 web development html explain basics HTML Html second lesson on html tutorial explain html more info information lesson2' ,
+
+  },
+  {
+    poster: ahdev,
+    articleName: 'HTML Project (Lesson 3)',
+    date: '17 / 12 / 2023 ',
+    article: 'Welcome to our final lesson in the HTML tutorial. In this lesson, we will delve into the world of basic HTML tags. <br> In this concluding lesson, we are going to create a small application to review fundamental HTML tags and comprehend how to utilize them in real-world projects. <br> Now, what is the idea behind the application? We will be building a TODO APP. This project type aids in organizing and planning tasks you intend to undertake. <br> Before we commence, I would like to share some insights about the project. In this venture, we will need to incorporate JavaScript and CSS. However, for this lesson, we will focus solely on establishing the HTML structure of the project. <br> The remaining CSS and JavaScript codes can be found in the final lesson of the JavaScript tutorial, where you can download the entire project and modify it to enhance the design. If you encounter any issues, feel free to reach out to me via email. <br> Now, let us dive into explaining the code. <iframe src="./proj.html" style="width:100%;height:500px;border-radius: 15px;border:1px solid gray" frameborder="0"></iframe><br><br> When crafting your own websites, it is advisable to leverage semantic HTML to sidestep potential issues and make the development process smoother in the long run. When collaborating with a team on any project, precision in code writing is crucial to ensure it is understandable to team members throughout the development process. <br> As you write code, you should be cognizant of the programming logic you aim to traverse during site development and have a plan for what you want to achieve over time in developing this site, regardless of its scale. <br> Now, I will elucidate why I utilized certain tags in the above code. For instance, by using the "input" tag, I employed it to gather user input. <br> I created a button to submit the data entered in the input, saving it for later review in the todo list. I prefer organizing the code by encapsulating each part within a div item to facilitate the development experience for myself. Lastly, let us delve into HTML attributes. <br> Attributes in HTML are among the most pivotal elements for executing actions on your site, such as creating links to navigate to other pages or sites, among various actions that elevate your site‘s sophistication. Here are some examples of HTML attributes: To style any HTML element on your page, you can easily employ the<code> style="" </code>attribute and directly write CSS code within it. Another example, if you wish to create a link for navigation between site pages, you can use the <a> tag, include the <code> href="" </code >attribute, and specify the page target in double quotes, and we’re done. I hope you derive benefit from this lesson, and remember, when you feel a lack of motivation, never give up. <br> <br> This tutorial has concluded. You can proceed to the CSS tutorial using this link <a href="../css/lesson1.html">👉 CSS Introduction 👈</a>' ,
+    shortArticle: 'Welcome to the last lesson in HTML tutorial , in this tutorial we learn HTML .<br> And in this lesson we will make a small app to review the HTML basic tags and understand how we can use it in real projects . ',
+    learnMoreLink: 'pages/Front-end Road Map/html/lesson3.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'HTML',
+    articleImag: '/Images/Articles Images /html.png' ,
+    lessonNumer: 'leeson 3' ,
+    roadMap : 'Front-end',
+    searchKeyWords: 'Lesson html Project project lesson3 web development html tags basics HTML Html first lesson on html tutorial tags review ' ,
+  },
+  {
+    poster: ahdev,
+    articleName: 'CSS Inderction (lesson 1)',
+    date: '26 / 02 / 2024 ',
+    article: `Welcome to the first lesson in the CSS tutorial , in this tutorial we will explain CSS and we will apply to a real project to get the best benefit from this tutorial. <br> Now what is the CSS ? CSS is a mark-up language built to format and style the HTML codes . <br> CSS appeared in 1968 by AHMAD AND THE CODE IS, and this language is very easy to learn . <br>So,  if you want to get best from this tutorial work with me on the project that‘s created in the <a href="../html/lesson3.html">last lesson in HTML tutorial</a> , so you can <a href="">download it</a> work with me on it . Let‘s start in this lesson .<br> <br> Now , what is the CSS code structure lool like ? The CSS code consists of two basic parts : the class name and the brackets <code> { } </code> that’s contents the properties of the selection element. <br> <br> . So , where we can writing the CSS codes ? Here we have several answers : we can write CSS between the <code> &lt;head&gt; &lt;/head&gt; </code> tags , or we can write it in the <code> style="" </code> attribute to make inline style , but you should know something about this , the properties you write in the tag attributes make overwrite for any property wrote in any where in your project for this element . And the last place you can write CSS codes is to write them in an external file that you can link to the HTML file via the link code. <br> So , we learned where we can write the CSS codes , now what is the CSS properties? The CSS properties are among the most important things that you should learn in this language because it enables you to control the properties of HTML element sush as shape, length, width and font size in it, and other things that you can use to provide a very creative and wonderful product.<br> This table contains most of the properties in CSS .   <br> <br> <h5 class="headers">CSS 3 Properties & Usage</h5> <br> <iframe src="./css properties.html" width = "100%" height = "1000px" frameborder="0"></iframe>  <br> ` ,
+    shortArticle: 'Welcome to the first lesson in the CSS tutorial , in this tutorial we will explain CSS and we will apply to a real project to get the best benefit from this tutorial. ',
+    learnMoreLink: 'pages/Front-end Road Map/css/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'CSS',
+    roadMap : 'Front-end',
+    articleImag: '/Images/Articles Images /css.png' ,
+    lessonNumer: 'leeson 1' ,
+    searchKeyWords: 'Lesson html Project project lesson3 web development html tags basics HTML Html first lesson on html tutorial tags review ' ,
+  },
+  {
+    poster: ahdev,
+    articleName: 'CSS Lesson 2',
+    date: '23 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/css/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'CSS',
+    roadMap : 'Front-end',
+    articleImag: '/Images/Articles Images /css.png' ,
+    lessonNumer: 'leeson 2'
+  },
+
+
+  {
+    poster: ahdev,
+    articleName: 'CSS Project (Lesson 3)',
+    date: '23 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/css/lesson1.html',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png',
+    langName: 'CSS',
+    roadMap : 'Front-end',
+    articleImag: '/Images/Articles Images /css.png' ,
+    lessonNumer: 'leeson 3'
+  },
+  {
+    poster: 'Rasheed Al-koas',
+    articleName: 'C++ lesson2',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'programming',
+        articleImag: 'Images/Developer logo .jpg' ,
+
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: 'Khalid Alek',
+    articleName: 'Redmi note 12',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'mobiles review',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: 'Khalid Alek',
+    articleName: 'Redmi note 12',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'mobiles review',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: 'Khalid Alek',
+    articleName: 'Redmi note 12',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'mobiles review',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: ahdev,
+    articleName: 'How to install VScode on Android',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'technology',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: ahdev,
+    articleName: 'How to install VScode on Android',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'technology',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: ahdev,
+    articleName: 'How to install VScode on Android',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'technology',
+    posterLogo: 'Images/Dev.png'
+  },
+  {
+    poster: ahdev,
+    articleName: 'Python lesson 1 (Basics)',
+    date: '6 / 10 / 2023 ',
+    article: 'blog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articlesblog specialist on technology and programming articles',
+    shortArticle: 'blog specialist on technology and programming articles',
+    learnMoreLink: 'pages/html/lesson2',
+    category: 'programming',
+    posterLogo: 'Images/Dev.png'
+  },
+]
